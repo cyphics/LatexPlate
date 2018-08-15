2cb177ab82454f62e89f378f2f6d4a85f399c994
cyphics <cyphics@protonmail.com>
2018-08-15 12:18:27 +0200

    clean and reorganize

--- add_in_between_item.sql
+++
@@ -1,7 +0,0 @@
-﻿--select * from TemplateEnvironment
-
-INSERT INTO TemplatePackage  (Template_FK, Package_FK)
-VALUES (
-	(select Nom from Template where Nom = 'Lettre_Officielle'), 
-	(select Nom from Package where Nom = 'linguex')
-	)
--- get_dependencies.sql
+++
@@ -1,2 +0,0 @@
-﻿select Package_FK from TemplatePackage
-where Template_FK = 'Lettre_Officielle'
